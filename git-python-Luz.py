4dbfd01a38e33d72cc486a289b646984b46be455
LuzChristinee <iamluzchristinee@gmail.com>
2022-03-14 12:05:21 +0800

    My Educational Background added

--- git-python-Luz.py
+++ git-python-Luz.py
@@ -1,3 +1,6 @@
 print("Christine Luz"); 
 print("Tampus,Boac,Marinduque"); 
 print("April 9, 2001"); 
+print("Marinduque State College"); 
+print("Bachelor of Science in Information Tachnology"); 
+print("Second year - Section D"); 

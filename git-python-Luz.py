8bc295bfabba995d66bab290c56d47faa816918a
LuzChristinee <iamluzchristinee@gmail.com>
2022-03-14 12:54:52 +0800

    My Hobbies added

--- git-python-Luz.py
+++ git-python-Luz.py
@@ -4,3 +4,8 @@ print("April 9, 2001");
 print("Marinduque State College"); 
 print("Bachelor of Science in Information Tachnology"); 
 print("Second year - Section D"); 
+print("Binge watching movies"); 
+print("Watch serial killer documentaries"); 
+print("Watch satisfying videos on the internet"); 
+print("Sleeping"); 
+print("Reading random books online"); 
